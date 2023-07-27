96cbf39f2752e1595f0ac22ae261d0ff5c9be73b
MaxWebDev <maximus3506@mail.ru>
2023-07-27 22:12:01 +0300

    add router-login guard3

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -42,7 +42,7 @@ export class LoginPageComponent implements OnInit{
       email: this.form.value.email,
       password: this.form.value.password
     }
-    this.auth.login(user).subscribe((response) => {
+    this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
       this.submitted = false
