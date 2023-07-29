9be4f7c346caa965eb4f62dfe4478e2eff3de3d9
MaxWebDev <maximus3506@mail.ru>
2023-07-29 21:54:54 +0300

    create interceptors

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -4,7 +4,7 @@ import {FbAuthResp, User} from "../../../shared/interfaces";
 import {catchError, Observable, Subject, tap, throwError} from "rxjs";
 import {environment} from "../../../../environments/Envenronment";
 
-@Injectable()
+@Injectable({providedIn: "root"})
 export class AuthService {
   public error$: Subject<string> = new Subject<string>()
   constructor(private http: HttpClient) {
