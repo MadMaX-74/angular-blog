908c734225fca9a138ae4c10cfe2cbe94631314e
MaxWebDev <maximus3506@mail.ru>
2023-07-29 23:14:08 +0300

    add edit post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Route} from "@angular/router";
+import {PostService} from "../../shared/post.service";
+import {switchMap} from "rxjs/operators";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent {
+export class EditPageComponent implements OnInit, OnDestroy{
+  form: FormGroup
+  post: Post
+  submitted = false
+  uSub: Subscription
+  constructor(private route: ActivatedRoute, private postService: PostService) {
+  }
 
+  ngOnInit(): void {
+    this.route.params
+        .pipe(switchMap((params) => {
+          return this.postService.getById(params['id'])
+        })).subscribe((post) => {
+          this.post = post
+          this.form = new FormGroup({
+             title: new FormControl(post.title, Validators.required),
+             text: new FormControl(post.text, Validators.required)
+          })
+    })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+    this.submitted = true
+    this.uSub = this.postService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.uSub && this.uSub.unsubscribe()
+  }
 }
