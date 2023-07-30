d023896ed0dedbeec9f3b17f3ce7105e30c45817
MaxWebDev <maximus3506@mail.ru>
2023-07-30 13:26:10 +0300

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import {NgModule, Provider} from '@angular/core';
+import {NgModule, Provider, isDevMode} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import {registerLocaleData} from "@angular/common";
 import ruLocale from "@angular/common/locales/ru";
 
@@ -33,7 +34,13 @@ const INTERCEPTOR_PROVIDER: Provider = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
