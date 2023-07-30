a506a87020c34325c6f344039b0102728a8e004e
MaxWebDev <maximus3506@mail.ru>
2023-07-30 13:00:01 +0300

    add post pages

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PostService} from "../shared/post.service";
+import {Observable, switchMap} from "rxjs";
+import {Post} from "../shared/interfaces";
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
-export class PostPageComponent {
+export class PostPageComponent implements OnInit{
+  post$: Observable<Post>
+  constructor(private route:ActivatedRoute, private postService: PostService) {
+  }
+  ngOnInit(): void {
+    this.post$ = this.route.params
+      .pipe(switchMap((params) => {
+        return this.postService.getById(params['id'])
+      }))
+  }
 
 }
